cb77a8b31d19b663eb9d91c381ea0fa0e90a2ebd
Will Holley <will@willholley.com>
2022-03-05 15:33:29 -0500

    `inputs.cmd`

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import core from "@actions/core";
 import github from "@actions/github";
+import exec from "@actions/exec";
 
-try {
-  // 1. Validate that the working directory has a hardhat config file
-  // 2. Check cache to determine whether a TCOD instance has already been created
-  // 2.1 No: Create a TCOD instance
-  // 2.2 Yes: Use endpoint from cache
-  // 3. Write the TCOD network endpoint to the config.
-  console.log("Hello World");
-} catch (error) {}
+(async () => {
+  try {
+    // Validate that the working directory has a hardhat config file
+
+    // Validate that the given arg is a valid Hardhat cmd
+    const cmd = core.getInput("cmd");
+    await exec.exec("yarn", ["hardhat", cmd]);
+
+    // 2. Check cache to determine whether a TCOD instance has already been created; alternative check https://github.com/actions/toolkit/tree/main/packages/core
+    // 2.1 No: Create a TCOD instance
+    // 2.2 Yes: Use endpoint from cache
+    // 3. Write the TCOD network endpoint to the config.
+  } catch (error) {
+    const message = (error as unknown as any).message as string;
+    core.setFailed(message);
+  }
+})();
