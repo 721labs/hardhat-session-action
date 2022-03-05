08e41bdcf67c849c8164c80eaeae21c76445a288
Will Holley <will@willholley.com>
2022-03-05 15:49:45 -0500

    Validate using `--help` and fail on error

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,14 @@ import { exec } from "@actions/exec";
     // Validate that the given arg is a valid Hardhat cmd
     const cmd = core.getInput("cmd");
     console.log(`$ yarn hardhat ${cmd}`);
-
-    await exec("yarn", ["hardhat", cmd]);
+    await exec("yarn", ["hardhat", cmd, "--help"]);
 
     // 2. Check cache to determine whether a TCOD instance has already been created; alternative check https://github.com/actions/toolkit/tree/main/packages/core
     // 2.1 No: Create a TCOD instance
     // 2.2 Yes: Use endpoint from cache
     // 3. Write the TCOD network endpoint to the config.
   } catch (error) {
-    console.log(error);
-    //const message = (error as unknown as any).message as string;
-    //core.setFailed(message);
+    const message = (error as unknown as any).message as string;
+    core.setFailed(message);
   }
 })();
