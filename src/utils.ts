1ca5c1ab046b74f12b59a93b256b945a458f836a
Will Holley <will@willholley.com>
2022-03-05 19:54:17 -0500

    Clearer labels

--- src/utils.ts
+++ src/utils.ts
@@ -8,15 +8,14 @@ async function updateHardhatConfig(cmd: string, sessionId: string) {
 
   // Check for passed in `--config` or `--tsconfig` flags
   const match = cmd.match(/--(ts)?config\s(?<path>.+)\s/);
-  //@ts-expect-error
-  core.info(match?.toString());
+  console.log("Dev: Match", match);
   if (match) filepath = match?.groups?.path as string;
   else {
     // Glob the filesystem
     const patterns = ["hardhat.config.ts", "hardhat.config.js"];
     const globber = await glob.create(patterns.join("\n"));
     const files = await globber.glob();
-    core.info(files.toString());
+    console.log("DEV: Global Files", files);
     if (files) filepath = files[0];
     else throw core.setFailed("Unable to parse Hardhat config");
   }
